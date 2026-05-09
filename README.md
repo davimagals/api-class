99368972fb0450afcadbea31d68d1cebee0427e0
Davi Magalhaes <davimagal.sc@gmail.com>
2026-05-09 11:05:49 -0300

    update

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 API criada para ensino de programação WEB e Mobile Híbrida no curso técnico em Desenvolvimento de Sistemas de uma Escola Profissional do Estado do Ceará.
 
 Oferece algumas rotas para diferentes contextos para diferentes exercícios.
+
+# Comandos
+
+- Subir:
+  docker compose up -d
+
+- Rebuilda o projeto:
+  docker compose up --build -d
+
+- Reset total (recria banco):
+  docker compose down -v
+  docker compose up -d
--- api/src/routes/drivers.router.js
+++ api/src/routes/drivers.router.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import { DriversController } from "../controllers/drivers.controller.js";
 import { validateBody } from "../middlewares/validate.middleware.js";
-// import { validateParams } from "../middlewares/validate.middleware.js";
 
 import { CreateDriverDTO } from "../dtos/driver/create-driver.dto.js";
 import { UpdateDriverDTO } from "../dtos/driver/update-driver.dto.js";
-// import { CnhParamDTO } from "../dtos/driver/cnh-param.dto.js";
 
 const router = Router();
 
